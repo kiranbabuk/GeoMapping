b4de36587401d9d0e4a5a8851dc8f92c9b5fca88
kiranbabuk <kiranbabukalluri@gmail.com>
2019-10-30 15:45:08 -0400

    corrected file path

--- static/js/logicbackup.js
+++ static/js/logicbackup.js
@@ -1,7 +1,5 @@
 
 var API_quakes="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
-//var API_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
-//var API_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 var API_plateboundaries = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 var API_plates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
